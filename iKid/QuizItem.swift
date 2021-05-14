90f3dd1f98007f3625081b3dd095e16397ecefe2
mackenziehutchison <mack2@uw.edu>
2021-05-13 21:09:11 -0700

    fixed questions

--- iKid/QuizItem.swift
+++ iKid/QuizItem.swift
@@ -57,10 +57,10 @@ class QuizItemStorage: ObservableObject {
                                                                                   questionItem(questionString: "A person would weigh more on earth than on the moon.",
                                                                                                answers: [answerItem(answerString: "True", isCorrect: true), answerItem(answerString: "False", isCorrect: false)])]),
                               quizQuestions(topic: "Random", description: "Random questions",
-                                                                      questions: [questionItem(questionString: "2+2=",
-                                                                                               answers: [answerItem(answerString: "4", isCorrect: true), answerItem(answerString: "22", isCorrect: false), answerItem(answerString: "3", isCorrect: false)]),
-                                                                                  questionItem(questionString: "2*2=",
-                                                                                               answers: [answerItem(answerString: "4", isCorrect: true), answerItem(answerString: "44", isCorrect: false), answerItem(answerString: "22", isCorrect: false)]),
-                                                                                  questionItem(questionString: "2*3=",
-                                                                                               answers: [answerItem(answerString: "6", isCorrect: true), answerItem(answerString: "23", isCorrect: false), answerItem(answerString: "66", isCorrect: false)])])]
+                                                                      questions: [questionItem(questionString: "What is the complementary color to blue?",
+                                                                                               answers: [answerItem(answerString: "orange", isCorrect: true), answerItem(answerString: "yellow", isCorrect: false), answerItem(answerString: "green", isCorrect: false)]),
+                                                                                  questionItem(questionString: "The first person convicted of speeding was going",
+                                                                                               answers: [answerItem(answerString: "8mph", isCorrect: true), answerItem(answerString: "30mph", isCorrect: false), answerItem(answerString: "100mph", isCorrect: false)]),
+                                                                                  questionItem(questionString: "What countries has the most spoken languages in the world?",
+                                                                                               answers: [answerItem(answerString: "Papua New Guinea", isCorrect: true), answerItem(answerString: "Spain", isCorrect: false), answerItem(answerString: "France", isCorrect: false)])])]
 }
